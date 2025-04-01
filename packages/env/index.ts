7922842c668c89c7c4a75e4841a77fde68fe75a3
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-04-01 11:42:16 -0300

    feat: adicionar variáveis de ambiente para configuração do Nodemailer

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -11,6 +11,11 @@ const env = createEnv({
     GOOGLE_OAUTH_CLIENT_ID: z.string(),
     GOOGLE_OAUTH_CLIENT_SECRET: z.string(),
     GOOGLE_OAUTH_REDIRECT_URI: z.string().url(),
+
+    NODEMAILER_SMTP_HOST: z.string(),
+    NODEMAILER_SMTP_PORT: z.coerce.number(),
+    NODEMAILER_SMTP_USER: z.string(),
+    NODEMAILER_SMTP_PASSWORD: z.string(),
   },
   client: {},
   shared: {
@@ -24,6 +29,10 @@ const env = createEnv({
     GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
     GOOGLE_OAUTH_REDIRECT_URI: process.env.GOOGLE_OAUTH_REDIRECT_URI,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NODEMAILER_SMTP_HOST: process.env.NODEMAILER_SMTP_HOST,
+    NODEMAILER_SMTP_PORT: process.env.NODEMAILER_SMTP_PORT,
+    NODEMAILER_SMTP_USER: process.env.NODEMAILER_SMTP_USER,
+    NODEMAILER_SMTP_PASSWORD: process.env.NODEMAILER_SMTP_PASSWORD,
   },
   emptyStringAsUndefined: true,
 })
