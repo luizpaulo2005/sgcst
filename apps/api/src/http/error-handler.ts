956154faded27b670fe9a1c5d2f259d1dfc3da24
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-04-01 21:12:06 -0300

    fix: atualizar manipulador de erro para usar validação de esquema do fastify-type-provider-zod

--- apps/api/src/http/error-handler.ts
+++ apps/api/src/http/error-handler.ts
@@ -1,5 +1,5 @@
 import type { FastifyInstance } from 'fastify'
-import { ZodError } from 'zod'
+import { hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod'
 
 import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
 import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
@@ -7,10 +7,19 @@ import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
 type FastifyErrorHandler = FastifyInstance['errorHandler']
 
 const errorHandler: FastifyErrorHandler = (error, request, reply) => {
-  if (error instanceof ZodError) {
+  if (hasZodFastifySchemaValidationErrors(error)) {
     return reply.status(400).send({
       message: 'Erro de validação.',
-      errors: error.flatten().fieldErrors,
+      errors: error.validation.map((validation) => {
+        return {
+          path: validation.params.issue.path[0],
+          message: validation.params.issue.message,
+          // @ts-expect-error - A propriedade "expected" existe em alguns casos.
+          expected: validation.params.issue.expected,
+          // @ts-expect-error - A propriedade "received" existe em alguns casos.
+          received: validation.params.issue.received,
+        }
+      }),
     })
   }
 
