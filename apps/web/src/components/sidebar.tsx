0324900a2945146b546a525388e3e6861729fd3c
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-04-22 11:25:28 -0300

    feat: atualizar sidebar para incluir novos itens de menu e gerenciar permissões de usuário

--- apps/web/src/components/sidebar.tsx
+++ apps/web/src/components/sidebar.tsx
@@ -1,7 +1,16 @@
-import { Home } from 'lucide-react'
+import {
+  AppWindow,
+  ChartLine,
+  Home,
+  Mail,
+  MapPin,
+  MessageSquare,
+  Users,
+} from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import { useAbility } from './providers/permissoes'
 import {
   Sidebar,
   SidebarContent,
@@ -15,25 +24,115 @@ import {
 
 const AppSidebar = () => {
   const path = usePathname()
+  const permissoes = useAbility()
 
   return (
     <Sidebar variant="inset" collapsible="icon">
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>SGCST</SidebarGroupLabel>
+          <SidebarGroupLabel>Chamados</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
+              {permissoes && permissoes.can('manage', 'Chamado') && (
+                <SidebarMenuItem>
+                  <SidebarMenuButton isActive={path === '/'} asChild>
+                    <Link href="/">
+                      <Home />
+                      Painel
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              )}
               <SidebarMenuItem>
-                <SidebarMenuButton isActive={path === '/'} asChild>
-                  <Link href="/">
-                    <Home />
-                    Painel
+                <SidebarMenuButton isActive={path === '/meus-chamados'} asChild>
+                  <Link href="/meus-chamados">
+                    <MessageSquare />
+                    Meus chamados
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
+              {permissoes && permissoes.can('manage', 'Chamado') && (
+                <SidebarMenuItem>
+                  <SidebarMenuButton isActive={path === '/chamados'} asChild>
+                    <Link href="/">
+                      <AppWindow />
+                      Chamados
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              )}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        {permissoes && permissoes.can('manage', 'Usuario') && (
+          <SidebarGroup>
+            <SidebarGroupLabel>Usuários</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton isActive={path === '/usuarios'} asChild>
+                      <Link href="/usuarios">
+                        <Users />
+                        Usuários
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton isActive={path === '/convites'} asChild>
+                      <Link href="/convites">
+                        <Mail />
+                        Convites
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
+        {permissoes && permissoes.can('manage', ['Categoria', 'Local']) && (
+          <SidebarGroup>
+            <SidebarGroupLabel>Configurações</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton isActive={path === '/categorias'} asChild>
+                    <Link href="/categorias">
+                      <Home />
+                      Categorias
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton isActive={path === '/locais'} asChild>
+                    <Link href="/locais">
+                      <MapPin />
+                      Locais
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
+        {permissoes && permissoes.can('manage', 'Chamado') && (
+          <SidebarGroup>
+            <SidebarGroupLabel>Dados</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton isActive={path === '/graficos'} asChild>
+                    <Link href="/graficos">
+                      <ChartLine />
+                      Gráficos
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
       </SidebarContent>
     </Sidebar>
   )
