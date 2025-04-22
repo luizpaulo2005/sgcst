570bb0eeddc53d46733c8c1da0602467bb6f255b
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-04-22 12:25:20 -0300

    refactor: remover middleware de autenticação e simplificar a rota de verificação de e-mail

--- apps/api/src/http/routes/auth/verificar-email.ts
+++ apps/api/src/http/routes/auth/verificar-email.ts
@@ -3,67 +3,62 @@ import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 
-import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 
 const verificarEmail = async (app: FastifyInstance) => {
-  app
-    .register(auth)
-    .withTypeProvider<ZodTypeProvider>()
-    .get(
-      '/auth/verificar-email',
-      {
-        schema: {
-          tags: ['autenticacao'],
-          summary: 'Verificar e-mail',
-          security: [{ bearerAuth: [] }],
-          querystring: z.object({
-            token: z.string(),
-          }),
-          response: {
-            204: z.null(),
-          },
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/auth/verificar-email',
+    {
+      schema: {
+        tags: ['autenticacao'],
+        summary: 'Verificar e-mail',
+        querystring: z.object({
+          token: z.string(),
+        }),
+        response: {
+          204: z.null(),
         },
       },
-      async (request, reply) => {
-        const { token } = request.query
+    },
+    async (request, reply) => {
+      const { token } = request.query
 
-        const tokenFromParams = await prisma.token.findFirst({
-          where: { id: token },
-        })
+      const tokenFromParams = await prisma.token.findFirst({
+        where: { id: token },
+      })
 
-        if (!tokenFromParams) {
-          throw new UnauthorizedError()
-        }
+      if (!tokenFromParams) {
+        throw new UnauthorizedError()
+      }
 
-        const dataAtual = new Date()
+      const dataAtual = new Date()
 
-        if (dataAtual > tokenFromParams.validoAte) {
-          throw new UnauthorizedError('Código expirado.')
-        }
+      if (dataAtual > tokenFromParams.validoAte) {
+        throw new UnauthorizedError('Código expirado.')
+      }
 
-        await prisma.$transaction([
-          prisma.usuario.update({
-            where: {
-              id: tokenFromParams.usuarioId,
-            },
-            data: {
-              emailVerificado: true,
-            },
-          }),
+      await prisma.$transaction([
+        prisma.usuario.update({
+          where: {
+            id: tokenFromParams.usuarioId,
+          },
+          data: {
+            emailVerificado: true,
+          },
+        }),
 
-          prisma.token.delete({
-            where: {
-              id: tokenFromParams.id,
-            },
-          }),
-        ])
+        prisma.token.delete({
+          where: {
+            id: tokenFromParams.id,
+          },
+        }),
+      ])
 
-        return reply.status(204).redirect(env.WEB_URL)
-      },
-    )
+      return reply.status(204).redirect(env.WEB_URL)
+    },
+  )
 }
 
 export { verificarEmail }
